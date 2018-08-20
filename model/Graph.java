40f4ab5189e8e435272c2d8326aa017e35ef38e9
Rolopens <rolopens@gmail.com>
2018-08-21 00:50:21 +0800

    Model Changes

--- model/Graph.java
+++ model/Graph.java
@@ -5,6 +5,9 @@
  */
 package model;
 import java.util.ArrayList;
+import java.util.Random;
+
+
 
 /**
  *
@@ -12,20 +15,77 @@ import java.util.ArrayList;
  */
 public class Graph {
     private ArrayList<Node> Nodes = new ArrayList<Node>();
+    private ArrayList<Node> startStates = new ArrayList<Node>();
+    private ArrayList<Node> finalStates = new ArrayList<Node>();
     private Node current; //where the graph is pointing at in the graph
+    
+    Random rand = new Random();
     
     public void appendNode(Node node){
         Nodes.add(node);
+        this.findFinalStates();
+        this.findStartStates();
         
+        //get random start state for the graph
+        int  n = rand.nextInt(startStates.size()) + 0;
+        current = startStates.get(n);
+        
+    }
+    
+    private void findFinalStates(){
+        for (int i = 0; i < Nodes.size(); i++){
+            if(Nodes.get(i).isFinal()){
+                finalStates.add(Nodes.get(i));
+            }
+        }
+    }
+    
+    private void findStartStates(){
+        for (int i = 0; i < Nodes.size(); i++){
+           if(Nodes.get(i).isStart()){
+                startStates.add(Nodes.get(i));
+            }
+        }
     }
 
     public ArrayList<Node> getNodes() {
         return Nodes;
     }
 
-
     public Node getCurrent() {
         return current;
     }
     
+    //function to be used when graph object will move the current node
+    //this function will be used for finding the solution
+    public ArrayList<Node> findSolution(ArrayList<Node> solutionPath){
+//        if(current.isFinal()){
+//            solutionPath.add(current);
+//            return solutionPath;
+//        }
+//        else{
+//            solutionPath.add(current);
+//            current.setIsVisited(true);
+//            for (int i = 0; i < current.getNext().size(); i++){
+//                if (current.getNext().get(i).getIsVisited() == false){
+//                    current = current.getNext().get(i);
+//                    break;
+//                }
+//            }
+//            return findSolution(solutionPath);
+//        }
+        return null;
+    }
+    
+    //function to be used when user inputs a character: human, human, lion, cow, grain
+    public void moveCurrentViaInput(String input){
+        
+    }
+    
+    //function to be used when user inputs 2 characters: human, human, lion, cow, grain
+    public void moveCurrentVia2Inputs(String input1, String input2){
+        
+    }
+    
 }
+
--- model/Node.java
+++ model/Node.java
@@ -11,16 +11,18 @@ import java.util.ArrayList;
  */
 public class Node {
     private int stateNumber;
-    private ArrayList<Node> pPrev = new ArrayList<Node>();
+    private ArrayList<Node> pNext = new ArrayList<Node>();
     private boolean startState;
     private boolean finalState;
     private ArrayList<String> inputs = new ArrayList<String>(); 
+    private boolean isVisited;
 
     public Node(int stateNumber, boolean startState, boolean finalState, ArrayList<String> inputs) {
         this.stateNumber = stateNumber;
         this.startState = startState;
         this.finalState = finalState;
         this.inputs = inputs;
+        isVisited = false;
     }
 
     public int getStateNumber(){
@@ -35,7 +37,19 @@ public class Node {
         return finalState;
     }
     
-    public void addPrev(Node node){
-        pPrev.add(node);
+    public void addNext(Node node){
+        pNext.add(node);
+    }
+    
+    public ArrayList<Node> getNext(){
+        return pNext;
+    }
+    
+    public boolean getIsVisited(){
+        return isVisited;
+    }
+    
+    public void setIsVisited(boolean value){
+        isVisited = value;
     }
 }
